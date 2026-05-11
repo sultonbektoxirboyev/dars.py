70bef45390c77e9bc1eac81479e59b88e2dc58a4
Sultonbek Tohirboyev <sultonbektoxirboyev@gmail.com>
2026-05-11 16:03:16 +0500

    Add files via upload

---
+++ dars.py
@@ -0,0 +1,26 @@
+maxsulotlar = ['olma','bulochka','xoddog','pechenya','cola','fanta','ruchka']
+print(f"Hush kelibsiz Bufetimizga bizda hozir {maxsulotlar} maxsulotlar bor.Sizga nima kerak?")
+def savdogar(maxsulot):
+    if maxsulot == 'olma':
+        print(f"Bizda {maxsulot} 5.000 so'm olasizmi?🎮")
+    elif maxsulot == 'bulochka':
+        print(f"Bizda {maxsulot} 7.000 so'm olasizmi?")
+    elif maxsulot == 'xoddog':
+        print(f"Bizda {maxsulot} 12.000 so'm olasizmi?😉")
+    elif maxsulot == 'pechenye':
+        print(f"Bizda {maxsulot} 3.000 so'm olasizmi?💰")
+    elif maxsulot == 'cola':
+        print(f"Bizda {maxsulot} 11.000 so'm olasizmi?💰")
+    elif maxsulot == 'fanta':
+        print(f"Bizda {maxsulot} 8.000 so'm olasizmi?😉💰")
+    elif maxsulot == 'ruchka':
+        print(f"Bizda {maxsulot} 1.000 so'm olasizmi?😉💰")
+print(savdogar('olma'))
+print(savdogar('bulochka'))
+print(savdogar('xoddog'))
+print(savdogar('pechenye'))
+print(savdogar('cola'))
+print(savdogar('fanta'))
+print(savdogar('ruchka'))
+
+
